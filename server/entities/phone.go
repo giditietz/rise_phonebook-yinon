bf6a91381c76d496786e99d5cac665fb00f238a5
yinon yishay <yishay.yinon@gmail.com>
2022-09-09 15:41:51 +0300

    remove binding for phone

--- server/entities/phone.go
+++ server/entities/phone.go
@@ -4,7 +4,7 @@ type PhoneQuery struct {
 	PhoneID     int    `json:"phoneID"`
 	ContactID   int    `json:"contactID"`
 	Description string `json:"description"`
-	PhoneNumber string `json:"PhoneNumber" binding:"iso3166_1_alpha2"`
+	PhoneNumber string `json:"PhoneNumber"`
 }
 
 type PhoneRequestBody struct {
